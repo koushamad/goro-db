2ea90a9c992e353259558d0c63a89c06573b4bef
kousha <kousha.ghodsizad@gmail.com>
2020-02-25 17:49:53 +0300

    fix bug

--- app/database/sql/sqlite/sqlite.go
+++ app/database/sql/sqlite/sqlite.go
@@ -3,14 +3,14 @@ package sqlite
 import (
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/koushamad/goro-app/app/helper"
 	"github.com/koushamad/goro-core/app/conf"
 	"github.com/koushamad/goro-core/app/exception/throw"
-	"github.com/koushamad/goro-core/app/helper"
 	"sync"
 )
 
 var (
-	once sync.Once
+	once   sync.Once
 	Sqlite *sqlite
 )
 
@@ -22,7 +22,7 @@ func Boot() *sqlite {
 	once.Do(func() {
 		db, err := gorm.Open(conf.Get("db.driver"), helper.SqlitePatch())
 		throw.Fatal("failed to connect database", 115, err)
-		Sqlite = &sqlite{DB:db}
+		Sqlite = &sqlite{DB: db}
 	})
 
 	return Sqlite
@@ -36,8 +36,8 @@ func (s *sqlite) Kill() {
 	defer s.DB.Close()
 }
 
-func (s *sqlite) Migrate(tables []interface{})  {
-	for _,t := range tables{
+func (s *sqlite) Migrate(tables []interface{}) {
+	for _, t := range tables {
 		s.DB.AutoMigrate(t)
 	}
 }
--- config/database.go
+++ config/database.go
@@ -1,16 +1,15 @@
 package config
 
-import "github.com/koushamad/goro-core/app/helper"
+import "github.com/koushamad/goro-app/app/helper"
 
 var (
 	// mysql | postgres | sqlite3
 	Database = map[string]string{
-		"driver": helper.Env("DATABASE_DRIVER", "sqlite3"),
-		"host": helper.Env("DATABASE_HOST",""),
-		"port": helper.Env("DATABASE_PORT",""),
-		"database": helper.Env("DATABASE_NAME","goro.db"),
-		"user": helper.Env("DATABASE_USER",""),
-		"pass": helper.Env("DATABASE_PASS",""),
+		"driver":   helper.Env("DATABASE_DRIVER", "sqlite3"),
+		"host":     helper.Env("DATABASE_HOST", ""),
+		"port":     helper.Env("DATABASE_PORT", ""),
+		"database": helper.Env("DATABASE_NAME", "goro.db"),
+		"user":     helper.Env("DATABASE_USER", ""),
+		"pass":     helper.Env("DATABASE_PASS", ""),
 	}
 )
-
