dae7d4b2f57b2cae7affc9f4d5d361aa57584fdc
kousha <kousha.ghodsizad@gmail.com>
2020-02-29 12:42:26 +0300

    change migration file

--- app/database/sql/mysql/mysql.go
+++ app/database/sql/mysql/mysql.go
@@ -9,7 +9,7 @@ import (
 )
 
 var (
-	once sync.Once
+	once  sync.Once
 	Mysql *mysql
 )
 
@@ -19,9 +19,9 @@ type mysql struct {
 
 func Boot() *mysql {
 	once.Do(func() {
-		db, err := gorm.Open(conf.Get("db.driver"), getConnection())
+		db, err := gorm.Open(conf.Get("db.driver"), getConnection)
 		throw.Fatal("failed to connect database", 114, err)
-		Mysql = &mysql{DB:db}
+		Mysql = &mysql{DB: db}
 	})
 
 	return Mysql
@@ -31,20 +31,19 @@ func (s *mysql) Init() *gorm.DB {
 	return Mysql.DB
 }
 
-func (s *mysql) Kill()  {
+func (s *mysql) Kill() {
 	defer s.DB.Close()
 }
 
 func getConnection() string {
 	return conf.Get("db.user") +
 		":" + conf.Get("db.pass") +
-		"@" + conf.Get(conf.Get("db.host")) +
-		":" + conf.Get(conf.Get("db.port")) +
-		"/" + conf.Get(conf.Get("db.name"))
+		"@" + conf.Get("db.host") + conf.Get("db.port") +
+		"/" + conf.Get("db.database")
 }
 
-func (s *mysql) Migrate(tables []interface{})  {
-	for _,t := range tables{
+func (s *mysql) Migrate(tables []interface{}) {
+	for _, t := range tables {
 		s.DB.AutoMigrate(t)
 	}
 }
